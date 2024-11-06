c105dd4f956b06ffade0708061c204fec86146da
Lee Cooper <lee@codebootup.com>
2024-11-06 15:36:00 +0800

    updated client to accomodate patch call

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -2,4 +2,5 @@ dependencies {
     api(project(":api"))
     api("org.springframework:spring-web")
     implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("org.apache.httpcomponents.client5:httpclient5")
 }
